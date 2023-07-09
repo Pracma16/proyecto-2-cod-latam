d3553f4b6c77be1e97d01ea59de2ce996065fdd1
pracma16 <ecoelectro16@gmail.com>
2023-07-09 13:30:13 -0300

    tercera entrega

--- js/main.js
+++ js/main.js
@@ -1,30 +1,20 @@
 
-/*aqui inicia la funcion 1*/
-/*const currentItem = document.querySelector('.foto2');
+const menuItems = document.querySelectorAll('.menu-item');
 menuItems.forEach(function(item) { 
     item.addEventListener('click', function(e) {
         const currentItem = document.querySelector('.active');
         currentItem.classList.remove('active');
         e.target.classList.add('active');
     });
-});*/
+});
 
-
-
-
-/*aqui finaliza la funcion 1*/
-
-
-/*aca inicia la funcion 2*/ 
 const btn = document.querySelector('.btn');
 const menu = document.querySelector('.menu');
 btn.addEventListener('click', () => {
     menu.classList.toggle('ver');
 }); 
-/*aqui finaliza la funcion 2*/
 
-
-const consultasItems = document.querySelectorAll('#consultas');
+const consultasItems = document.querySelectorAll('.menu');
 consultasItems.forEach(function(item) {
     item.addEventListener('click', function(e) {
         const activeItem = document.querySelector('.active');
@@ -34,12 +24,3 @@ consultasItems.forEach(function(item) {
         e.target.classList.add('active');
     });
 });
-/*En este ejemplo, hemos creado tres botones con la clase .foto. Al hacer clic en uno de los botones, se aplicará la clase .active al botón seleccionado, lo que cambiará su color de fondo a rojo según las reglas CSS definidas.
-
-Recuerda colocar el archivo CSS en la misma ubicación que el archivo HTML y el archivo JavaScript en la etiqueta <head> del archivo HTML.*/
-
-
-
-
-
-
